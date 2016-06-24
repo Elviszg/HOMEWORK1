a61dcfe9f5e16f4abdf069ade46cd84ea046077f
Elviszg <elviszgordillo@gmail.com>
2016-06-24 13:23:39 -0500

    Update ViewController.swift

--- pruebaTarea1/ViewController.swift
+++ pruebaTarea1/ViewController.swift
@@ -13,6 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //HOMEWORK 1
+        
+        var numeros = 1...100
+        for numero in numeros{
+        //Numero divisible entre 5
+        //Imprimir (# + Bingo!!!)
+            if(numero%10 == 5 || numero%10 == 0)
+                {
+                    print("\(numero) Bingo!!!")
+                }
+                else if(numero%2 == 0 )
+                {
+                    print("\(numero) Par!!!")
+                }
+                else if(numero%3 != 0){
+                    print("\(numero) Impar!!!")
+                }
+                else if numero >= 30 && numero <= 40 {
+                    print ("\(numero) Viva Swift!!!")
+                }
+            }
     }
 
     override func didReceiveMemoryWarning() {
